74227c14b9f52bf6f28a7af0ef54cdd6b4ef1aa9
Deepak Kumar <106384395+deepak1967@users.noreply.github.com>
2023-09-14 12:36:20 +0000

    #

---
+++ basic/variables.js
@@ -0,0 +1,14 @@
+const accountId = 45952;
+let accountEmail = "dpsingh87@gmail.com";
+var accountPassword = "659865";
+accountCity = "Jaipur";
+
+// accountId = 6566868; // not allow because it is constant value.
+
+accountEmail = "minnu7648@gmail.com";
+accountPassword = "698356";
+accountCity = "Noida";
+
+
+console.log("accountId");
+console.table([accountId, accountEmail, accountPassword, accountCity]);
