42f37c3d72761820d4adff300331ea51f682ee2a
Deepak Kumar <106384395+deepak1967@users.noreply.github.com>
2023-09-14 13:52:00 +0000

    #

---
+++ basic/datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ); // we are using nodejs, not browser
+
+console.log( 3 + 3);
+
+console.log("Deepak");
+
+let age = 34;
+let name = "Deepak";
+let loggedIn = false;
+
+// Datatypes in JS
+//primitive
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// non-primitive
+
+// object
+
+console.log(typeof age); // 34
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
