628ad4d9a2f1d86684890c94061a9eddfe386215
Deepak Singh <deepsingh262002@gmail.com>
2025-07-04 19:59:00 +0530

    calculate power

--- basicProblems/calculatePower.js
+++ basicProblems/calculatePower.js
@@ -1,5 +1,5 @@
 var calculatePower = function (base, num) {
-    let power = 0;
+  let power = 0;
   for (let i = 0; i <= num; i++) {
     while (num % base == 0) {
       num = num / base;
@@ -12,11 +12,20 @@ var calculatePower = function (base, num) {
 
 console.log(calculatePower(3, 81));
 
-// calculate number using recursion
+// calculate power using recursion
 
-var calculatePower = function(base, power){
-  if(power === 0) return 1;
-  return base * calculatePower(base, power - 1)
-}
+var calculatePower = function (base, power) {
+  if (power === 0) return 1;
+  return base * calculatePower(base, power - 1);
+};
+
+console.log(calculatePower(3, 4));
+
+// calculate power using Math function
+
+var calculatePower = function (base, power) {
+  let num = Math.pow(base, power);
+  return num;
+};
 
 console.log(calculatePower(3, 4));
