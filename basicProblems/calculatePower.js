9e5d79212551d738cec8357feb214890c7c2a094
Deepak Singh <deepsingh262002@gmail.com>
2025-07-04 18:06:08 +0530

    call function using recurion or itself

--- basicProblems/calculatePower.js
+++ basicProblems/calculatePower.js
@@ -1,4 +1,4 @@
-let calculatePower = function (base, num) {
+var calculatePower = function (base, num) {
     let power = 0;
   for (let i = 0; i <= num; i++) {
     while (num % base == 0) {
@@ -10,4 +10,13 @@ let calculatePower = function (base, num) {
   return power;
 };
 
-console.log(calculatePower(3, 9));
+console.log(calculatePower(3, 81));
+
+// calculate number using recursion
+
+var calculatePower = function(base, power){
+  if(power === 0) return 1;
+  return base * calculatePower(base, power - 1)
+}
+
+console.log(calculatePower(3, 4));
