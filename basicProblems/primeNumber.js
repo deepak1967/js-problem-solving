d0183015a6419e25e1073c326248c4535e61d3e8
Deepak Singh <deepsingh262002@gmail.com>
2025-07-06 18:54:53 +0530

    print prime number

--- basicProblems/primeNumber.js
+++ basicProblems/primeNumber.js
@@ -1,15 +1,32 @@
-function isPrime(num) {
-    for (let i = 2; i < num; i++) {
-        if (num % i == 0) {
-            return false;
-        }
-
+var isPrime = function (num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) {
+      return false;
     }
+  }
 
-    return true;
-}
+  return true;
+};
 
 console.log(isPrime(130));
 
+// Print prime numbers
 
+var primeNumber = function (num) {
+  let arr = [];
+  for (let i = 0; i < num; i++) {
+    let flag = 0;
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) {
+        flag = 1;
+      }
+    }
 
+    if (i > 1 && flag == 0) {
+      arr.push(i);
+    }
+  }
+  return arr;
+};
+
+console.log(primeNumber(10));
