32aac888c528a2c50ec4340343c34e190c832d57
deepak <dpsingh0804@gmail.com>
2025-07-10 21:15:02 +0530

    also check string palindrome

--- leetcode_problems/easy/palindrome.js
+++ leetcode_problems/easy/palindrome.js
@@ -1,7 +1,30 @@
-var isPalindrome = function (x) {
-   const num = +x.toString().split("").reverse().join("");    
-   return num === x
+var isPalindrome = function (num) {
+   const number = +num.toString().split("").reverse().join("");
+   return number === num
 };
 
-const data = isPalindrome(124);
-console.log(data);
+console.log(isPalindrome(121));
+
+var isPalindrome = function (string) {
+   const str = string.split("").reverse().join("");
+   return str === string
+};
+
+console.log(isPalindrome("nitin"));
+
+var isPalindrome = function (string) {
+   let str = string;
+   let arr = [];
+
+   for (let i = str.length - 1; i >= 0; i--) {
+      arr.push(str[i])
+   }
+
+   const joinStr = arr.join("");
+
+   return string === joinStr;
+
+};
+
+console.log(isPalindrome("nitin"));
+
