61b01009cad07d6b84ef3cf6b2c140e79ac60ee9
deepak <dpsingh0804@gmail.com>
2025-07-10 20:38:59 +0530

    find ASCII value

---
+++ basicProblems/asciiValue.js
@@ -0,0 +1,9 @@
+var asciiValue = function (string) {
+    const result = string.charCodeAt(0);
+    // OR
+    // const result = string.codePointAt(0)
+
+    return result
+}
+
+console.log(findAsciiValue('abc'));
