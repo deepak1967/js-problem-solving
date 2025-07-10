5e631285c10b5f9ff7d9f05425e5584e8bb34bf5
deepak <dpsingh0804@gmail.com>
2025-07-10 20:29:42 +0530

    update lcm function

--- basicProblems/calculateLCM.js
+++ basicProblems/calculateLCM.js
@@ -1,40 +1,29 @@
 // Formula: LCM(a, b) = (a * b) / HCF(a, b);
 
 var calculateLCM = function (a, b) {
-  let max = a > b ? a : b;
-  let min = a > b ? b : a;
-  let hcf = 1,
-    rem = 1,
-    lcm = 1;
-  for (let i = 0; i <= min || i <= max; i++) {
-    if (max % min != 0) {
-      rem = max % min;
-      max = min;
-      min = rem;
-      hcf *= rem;
-    } else {
-      hcf = min;
-      return hcf;
+  let hcf = 1
+  for (let i = 0; i < a || i < b; i++) {
+
+    if (a % i == 0 && b % i == 0) {
+      hcf = i;
     }
   }
   lcm = (a * b) / hcf;
   return lcm;
 };
 
+console.log(calculateLCM(12, 15));
+
+
 var calculateLCM = function (num1, num2) {
-  let count = 2,
-    lcm = 1;
-  while (count <= num1 && count <= num2) {
-    for (let i = 0; i <= num1 && i <= num2; i++) {
-      if (num1 % count == 0 || num2 % count == 0) {
-        if (num1 % count == 0) num1 = num1 / count;
-        if (num2 % count == 0) num2 = num2 / count;
-        lcm *= count;
-      }
+  let min = (num1 > num2) ? num1 : num2;
+  while (true) {
+    if (min % num1 == 0 && min % num2 == 0) {
+      break;
     }
-    count++;
+    min++;
   }
-  return lcm;
+  return min;
 };
 
-console.log(calculateLCM(100, 80));
+console.log(calculateLCM(12, 15));
