8c28a2491f7a1dc6dd82a43464d889f43c3aa32d
deepak <dpsingh0804@gmail.com>
2025-07-10 20:14:58 +0530

    update hfc function

--- basicProblems/gcd.js
+++ basicProblems/gcd.js
@@ -1,20 +1,15 @@
 var calculateHCF = function (num1, num2) {
-  let arr = [];
-  let hcf = 1,
-    count = 1;
+  let hcf = 1
   for (let i = 0; i < num1 || i < num2; i++) {
-    if (num1 % count == 0 && num2 % count == 0) {
-      num1 = num1 / count;
-      num2 = num2 / count;
-      arr.push(count);
-      hcf = hcf * count;
+
+    if (num1 % i == 0 && num2 % i == 0) {
+      hcf = i;
     }
-    count++;
   }
   return hcf;
 };
 
-console.log(calculateHCF(3, 6));
+console.log(calculateHCF(12, 18));
 
 var calculateHCF = function (a, b) {
   let max = a > b ? a : b;
