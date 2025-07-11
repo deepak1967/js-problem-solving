9ef1060d0f700f721fc1f6ccfbc379521931bed9
deepak <dpsingh0804@gmail.com>
2025-07-11 15:25:54 +0530

    update fibonacci series function

--- leetcode_problems/easy/fibonacci.js
+++ leetcode_problems/easy/fibonacci.js
@@ -1,24 +1,24 @@
 // fibonacci series = 0, 1, 1, 2, 3, 5, 8, 13, 21......
 
-// var fib = function (n) {
-//     const arr = [0, 1];
-//     for (let i = 2; i < 10; i++) {
-//         arr[i] = arr[i - 1] + arr[i - 2];
-//         arr.push(arr[i]);
-//     }
-//     return arr[n];
-// }
+var fib = function (n) {
+    const arr = [0, 1];
+    for (let i = 2; i < n; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr;
+}
 
-//  OR 
+console.log(fib(10));
 
-// var fib = (n) => {
-//     if(n<=1) return n;
-//     return fib(n-1) + fib(n-2);
-// }
 
-//  OR 
 
-var fib = (n) => n<=1 ? n: fib(n-1) + fib(n-2);
+var fib = (n) => {
+    if (n <= 1) return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+
+var fib = (n) => n <= 1 ? n : fib(n - 1) + fib(n - 2);
 
 
 console.log(fib(3));
