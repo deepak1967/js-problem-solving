7c7c8d32537b1d308b5da406c1cc9e270f00744e
deepak <dpsingh0804@gmail.com>
2025-07-15 05:49:46 +0530

    one more method to calculate hcf

--- basicProblems/gcd.js
+++ basicProblems/gcd.js
@@ -11,6 +11,17 @@ var calculateHCF = function (num1, num2) {
 
 console.log(calculateHCF(12, 18));
 
+var calculateHCF = function (num1, num2) {
+  while (num2 !== 0) {
+    let temp = num2;
+    num2 = num1 % num2;
+    num1 = temp;
+  }
+  return num1;
+}
+
+console.log(calculateHCF(12, 18));
+
 var calculateHCF = function (a, b) {
   let max = a > b ? a : b;
   let min = a > b ? b : a;
