97e9aeffaa6862e47f0843e1c0a22e9f5a7b8087
deepak <dpsingh0804@gmail.com>
2025-07-16 20:06:18 +0530

    chack anagram

--- leetcode_problems/easy/anagram.js
+++ leetcode_problems/easy/anagram.js
@@ -7,3 +7,28 @@ var isAnagram = function (s, t) {
 };
 
 console.log(isAnagram("car", "rac"));
+
+var isAnagram = function (str1, str2) {
+    let s1 = str1.split("");
+    let s2 = str2.split("");
+
+    for (let i = 0; i < s1.length && i < s2.length; i++) {
+        for (let j = i + 1; j < s1.length && j < s2.length; j++) {
+            if (s1[i] > s1[j]) {
+                let temp = s1[i];
+                s1[i] = s1[j];
+                s1[j] = temp
+            }
+            if (s2[i] > s2[j]) {
+                let temp = s2[i];
+                s2[i] = s2[j];
+                s2[j] = temp
+            }
+        }
+
+    }
+
+    return s1.join("") === s2.join("");
+}
+
+console.log(isAnagram("deepak", "kapdee"));
