268fcf7fb7b950ae0ffa5b82ecc0b2bbaafd7d62
deepak <dpsingh0804@gmail.com>
2025-07-18 09:19:28 +0530

    implement another method

--- basicProblems/rotateLeft.js
+++ basicProblems/rotateLeft.js
@@ -1,27 +1,30 @@
 let arr = [1, 2, 3, 4, 5];
 
 var rotateLeft = function (d, arr) {
+    let leftArr = arr.slice(0, d);
+    let rightArr = arr.slice(d, arr.length);
+    let nums = [...rightArr, ...leftArr];
+    return nums
+}
+
+console.log(rotateLeft(4, arr));
+
+var rotateLeft = function (d, arr) {
     let num = [...arr];
     num.splice(0, d);
 
     for (let i = 0; i < d; i++) {
         num.push(arr[i]);
     }
-
     return num;
 }
-
-
 
 console.log(rotateLeft(4, arr));
 
 var rotateLeft = function (d, arr) {
     const n = arr.length;
     d = d % n;
-
     return arr.slice(d).concat(arr.splice(0, d));
 }
-
-
 
 console.log(rotateLeft(4, arr));
