864a8266bd71667b8c24a4d935eb206cf620d19d
deepak <dpsingh0804@gmail.com>
2025-07-18 07:00:09 +0530

    update largest number

--- leetcode_problems/medium/largestNumber.js
+++ leetcode_problems/medium/largestNumber.js
@@ -1,13 +1,17 @@
-let nums = [3, 30, 34, 5, 9];
+let nums = [0, 0];
 
 var largestNumber = function (nums) {
     let arr = nums.sort((a, b) => {
         const order1 = `${a}${b}`;
         const order2 = `${b}${a}`;
+        if (a == b) return 0;
         return order2.localeCompare(order1);
     });
+    let result = arr.join("");
 
-    return arr.join("");
+    if (result[0] === '0') result = '0';
+
+    return result;
 };
 
 console.log(largestNumber(nums));
