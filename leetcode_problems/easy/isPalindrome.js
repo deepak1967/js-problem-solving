c36b63ec1ad03eefe73127bc9792c8011e9fb46d
deepak <dpsingh0804@gmail.com>
2025-07-18 21:05:00 +0530

    check palindrome

---
+++ leetcode_problems/easy/isPalindrome.js
@@ -0,0 +1,10 @@
+s = "A man, a plan, a canal: Panama"
+var isPalindrome = function (s) {
+    let string = s.toLowerCase().replace(/[^a-z0-9]/g, '');    
+    let reverse = string.split("").reverse().join("");
+    console.log(string, reverse);
+    
+    return string === reverse;
+};
+
+console.log(isPalindrome("0P"));
