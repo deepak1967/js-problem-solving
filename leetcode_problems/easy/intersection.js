aeaaf3742822453453970335fb0897f6eaa1a6f5
deepak <dpsingh0804@gmail.com>
2025-07-22 21:28:32 +0530

    check one array element exists in another aaray

---
+++ leetcode_problems/easy/intersection.js
@@ -0,0 +1,16 @@
+let nums1 = [4, 9, 5], nums2 = [9, 4, 9, 8, 4]
+
+var intersection = function (nums1, nums2) {
+    let arr = [];
+    for (let i = 0; i < nums1.length; i++) {
+        for (let j = 0; j < nums2.length; j++) {
+            if (nums1[i] === nums2[j]) {
+                if (!arr.includes(nums1[i])) arr.push(nums1[i])
+            };
+        }
+
+    }
+    return arr;
+};
+
+console.log(intersection(nums1, nums2));
