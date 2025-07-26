7eedac17f6397aebebb89527c08c1a31c7b6fbac
deepak <dpsingh0804@gmail.com>
2025-07-26 09:28:45 +0530

    define and example of infinite currying

--- basicProblems/currying.js
+++ basicProblems/currying.js
@@ -1,11 +1,11 @@
 // Currying is the process of converting a function with multiple arguments into a series of functions that each take one argument.
 
 var currying = function (a) {
-    return function (b) {
-        return function (c) {
-            return a + b + c;
-        }
+  return function (b) {
+    return function (c) {
+      return a + b + c;
     }
+  }
 }
 
 console.log(currying(1)(2)(3));
@@ -13,3 +13,15 @@ console.log(currying(1)(2)(3));
 var currying = a => b => c => a * b * c;
 
 console.log(currying(1)(2)(3));
+
+// Infinite Currying: A function that keeps returning functions to take arguments until a final call ends the chain and returns a result.
+
+function add(a) {
+  return function next(b) {
+    if (b === undefined) return a;
+    a += b;
+    return next;
+  };
+}
+console.log(add(1)(2)(3)(4)(5)()); // Output: 15
+
