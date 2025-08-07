b77a3b107b4b6fab832ad286a704e00ac9120fe6
deepak <dpsingh0804@gmail.com>
2025-08-07 17:57:41 +0530

    remove duplicate in array

--- basicProblems/removeDuplicate.js
+++ basicProblems/removeDuplicate.js
@@ -1,3 +1,19 @@
+let num = [2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3];
+
+function removeDuplicate(num) {
+    for (let i = 0; i < num.length; i++) {
+        for (let j = i + 1; j < num.length; j++) {
+            if (num[i] === num[j]) {
+                num.splice(j, 1);
+            }
+        }
+    }
+    return num;
+}
+
+console.log(removeDuplicate(num))
+
+
 let arr = [
     { id: 1, name: "deepak" },
     { id: 2, name: "lavi" },
